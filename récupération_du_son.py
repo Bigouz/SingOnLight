a5433e739af487edaa8fdf510bcb35207a72cbf5
Bigouz <louneshanane@gmail.com>
2025-11-17 12:50:15 +0100

    Update récupération_du_son.py

--- récupération_du_son.py
+++ récupération_du_son.py
@@ -2,5 +2,41 @@
 
 import pyaudio
 import numpy as np
+import time
 
-"Parame
+#Enregistrement de la données
+
+CHUNK = 1000 #Definition de la taille de l'échantillon audio
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 # 1 pour mono et 2 pour stéréo
+RATE = 44100
+
+#Ouverture du capteur son
+
+p = pyaudio.PyAudio()
+son = p.open(format = FORMAT,
+             channels = CHANNELS,
+             rate = RATE,
+             input = True,
+             frame_per_buffer = CHUNK)
+
+#Lire le son
+try:
+  While True:
+  donee = son.read(CHUNK) # lire le morceau du son
+  audio_donee = np.frombuffer(donee, dtype = np.int16) # Convertir en nombres utilisables
+  volume = np.sqrt(np.mean(audio_data**2)) # Calculer le volume
+
+  #afficher le volume
+  print(f"Volume : {volume}")
+  
+  # détécter si quelqu'un parle
+  if volume > 1000:
+    print("ça parle")
+
+except KeyboardInterrupt:
+  print("arret du programme")
+  son.stop_stream()
+  son.close()
+  p.terminate()
+
