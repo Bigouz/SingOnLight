4ea7adb84aa186fccb2ac009648df585a2ed347b
Sedique1 <mickaelwangpro916@gmail.com>
2025-11-19 15:44:14 +0100

    Update récupération_du_son.py

--- récupération_du_son.py
+++ récupération_du_son.py
@@ -6,13 +6,39 @@ import time
 import SoundSensor.py as sound
 
 # definition du décibel minimal a dépasser
-def recuperation son (n) : 
-  n # durée en seconde définit par l'utilisateur dans les paramètre
+def recuperation son (n) :
   l = []
   start = time.time()
   while time.time() - start<n:
     l.append(sound.main())
   return l
+
+def seuil(bruit):
+  phrase_calibrage = "Veuillez ne pas faire de bruit durant le calibrage du son ambiant"
+  phrase_commence = "Silence"
+  phrase_fin = "Calibrage du son ambiant terminé"
+  liste_Calibrage = []
+  for i in range (2):
+    n_ambiant = 5 # durée en seconde définit par l'utilisateur dans les paramètre
+    l = []
+    print(phrase_calibrage)
+    time.sleep(5)
+    for i in range(3,0,-1):
+      print("Le calibrage commence dans ",i," secondes")
+      time.sleep(1)
+    print(phrase_commence)
+    start = time.time()
+    while time.time() - start<n:
+      l.append(sound.main())
+    S = np.sqrt(np.mean(l**2)) # Calculer le Seuil bas
+    liste_calibrage.append(S)
+    print(phrase_fin)
+    phrase_calibrage = "Veuillez faire du bruit durant le calibrage du son joueur"
+    phrase_commence = "Crier !!!"
+    phrase_fin = "Calibrage du son joueur terminé"
+  
+  
+  
 
 """
 CHUNK = 1000 #Definition de la taille de l'échantillon audio
